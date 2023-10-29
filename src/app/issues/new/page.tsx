6c3941e5ded9f037dd0842b982663199cc53b5c5
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-10-29 22:21:51 +0800

    Add markdown editor

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from "react";
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMdeReact from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
     return (
@@ -9,7 +10,7 @@ const NewIssuePage = () => {
             <TextField.Root>
                 <TextField.Input placeholder="Title" />
             </TextField.Root>
-            <TextArea placeholder="Description" />
+            <SimpleMdeReact placeholder="Description" />
             <Button>Submit new issue</Button>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <div>Hello world</div>
-  )
+    return <div>Hello world</div>;
 }

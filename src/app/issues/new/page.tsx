0d17e454483c09ccffe36f9f55be1702c00953bb
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-10-31 19:15:22 +0800

    add a spinner

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -17,6 +17,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -31,6 +32,7 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema),
     });
     const [error, setError] = useState("");
+    const [isSubmitting, setSubmitting] = useState(false);
 
     return (
         <div className="max-w-xl">
@@ -43,9 +45,11 @@ const NewIssuePage = () => {
                 className="space-y-3"
                 onSubmit={handleSubmit(async (data) => {
                     try {
+                        setSubmitting(true);
                         await axios.post("/api/issues", data);
                         router.push("/issues");
                     } catch (error) {
+                        setSubmitting(false);
                         setError("An error occurred.");
                     }
                 })}
@@ -69,7 +73,10 @@ const NewIssuePage = () => {
 
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-                <Button>Submit New Issue</Button>
+                <Button disabled={isSubmitting}>
+                    Submit New Issue
+                    {isSubmitting && <Spinner />}
+                </Button>
             </form>
         </div>
     );

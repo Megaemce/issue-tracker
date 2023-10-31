bf0213053e1f16ab80aceff95c5a2cb577f23bc9
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-10-31 19:08:09 +0800

    Extract error messsage component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -16,6 +16,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -55,11 +56,9 @@ const NewIssuePage = () => {
                         {...register("title")}
                     />
                 </TextField.Root>
-                {errors.title && (
-                    <Text color="red" as="p">
-                        {errors.title.message}
-                    </Text>
-                )}
+
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
                 <Controller
                     name="description"
                     control={control}
@@ -67,11 +66,9 @@ const NewIssuePage = () => {
                         <SimpleMDE placeholder="Description" {...field} />
                     )}
                 />
-                {errors.description && (
-                    <Text color="red" as="p">
-                        {errors.description.message}
-                    </Text>
-                )}
+
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
                 <Button>Submit New Issue</Button>
             </form>
         </div>
